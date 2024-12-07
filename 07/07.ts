60944c43cecc0f7b4402fae2b0494574740770d8
Manuel <manuel.trageser789@googlemail.com>
2024-12-07 13:38:13 +0100

    day 7 solution

---
+++ 07/07.ts
@@ -0,0 +1,79 @@
+import PriorityQueue from "ts-priority-queue";
+import { FileReader } from "../utils";
+
+interface Equation {
+  desiredResult: number;
+  numbers: number[];
+}
+
+interface QueueItem {
+  partialResult: number;
+  equationIdx: number;
+}
+
+function checkSolvable(equation: Equation, allowConcat: boolean = false) {
+  let queue = new PriorityQueue({
+    comparator: function (a: QueueItem, b: QueueItem) {
+      return b.partialResult - a.partialResult;
+    },
+    initialValues: [{ partialResult: equation.numbers[0], equationIdx: 0 }],
+  });
+
+  while (queue.length) {
+    const biggestPartResultItem = queue.dequeue();
+    if (
+      biggestPartResultItem.equationIdx === equation.numbers.length - 1 &&
+      biggestPartResultItem.partialResult === equation.desiredResult
+    ) {
+      return true;
+    } else if (
+      biggestPartResultItem.partialResult <= equation.desiredResult &&
+      !(biggestPartResultItem.equationIdx === equation.numbers.length - 1)
+    ) {
+      const newIdx = biggestPartResultItem.equationIdx + 1;
+      const newPartialResultAdd =
+        biggestPartResultItem.partialResult + equation.numbers[newIdx];
+      const newPartialResultMultiply =
+        biggestPartResultItem.partialResult * equation.numbers[newIdx];
+
+      queue.queue({ partialResult: newPartialResultAdd, equationIdx: newIdx });
+      queue.queue({
+        partialResult: newPartialResultMultiply,
+        equationIdx: newIdx,
+      });
+      if (allowConcat) {
+        const newPartialResultConcat = Number(
+          "" + biggestPartResultItem.partialResult + equation.numbers[newIdx]
+        );
+        queue.queue({
+          partialResult: newPartialResultConcat,
+          equationIdx: newIdx,
+        });
+      }
+    }
+  }
+  return false;
+}
+
+const eqs: string[] = FileReader.ReadRowByRow("input.txt");
+let equations: Equation[] = [];
+for (const eq of eqs) {
+  const splitResult = eq.split(":");
+  const result = Number(splitResult[0]);
+  const numbers = splitResult[1]
+    .trim()
+    .split(" ")
+    .map((value: string) => Number(value));
+  equations.push({ desiredResult: result, numbers: numbers });
+}
+
+let solvableSum: number = 0;
+let solvableSumConcat: number = 0;
+for (const equation of equations) {
+  if (checkSolvable(equation)) solvableSum += equation.desiredResult;
+  if (checkSolvable(equation, true))
+    solvableSumConcat += equation.desiredResult;
+}
+
+console.log(`Part 1 solution: ${solvableSum}`);
+console.log(`Part 2 solution: ${solvableSumConcat}`);
