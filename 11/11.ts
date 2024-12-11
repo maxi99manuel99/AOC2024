62664f9e5eb60e432a6ac4b4de386b9ac5e4880e
Manuel <manuel.trageser789@googlemail.com>
2024-12-11 14:34:22 +0100

    a little bit of caching everything is blazing fast + variable naming and comments

--- 11/11.ts
+++ 11/11.ts
@@ -2,21 +2,52 @@ import { FileReader } from "../utils";
 
 const stones = FileReader.readAs2DMap("input.txt", " ")[0] as string[];
 
-function blink(stone: string, amount: number): number {
-  if (amount === 0) return 1;
-  else if (stone === "0") return blink("1", amount - 1);
-  else if (stone.length % 2 !== 0)
-    return blink("" + Number(stone) * 2024, amount - 1);
-  else
-    return (
-      blink("" + Number(stone.slice(0, stone.length / 2)), amount - 1) +
-      blink("" + Number(stone.slice(stone.length / 2)), amount - 1)
+const globalBlinkCache: Map<string, number> = new Map();
+
+/**
+    Performs [timesToBlink] blink operations on a stone and returns the amount of stones that are created by doing so
+ */
+function blinkAndCountStones(stone: string, timesToBlink: number): number {
+  const cacheKey = `${stone}:${timesToBlink}`;
+
+  if (globalBlinkCache.has(cacheKey)) {
+    return globalBlinkCache.get(cacheKey)!;
+  }
+
+  let stonesCreated: number;
+  if (timesToBlink === 0) {
+    stonesCreated = 1;
+  } else if (stone === "0") {
+    stonesCreated = blinkAndCountStones("1", timesToBlink - 1);
+  } else if (stone.length % 2 !== 0) {
+    stonesCreated = blinkAndCountStones(
+      `${Number(stone) * 2024}`,
+      timesToBlink - 1
     );
+  } else {
+    stonesCreated =
+      blinkAndCountStones(
+        `${Number(stone.slice(0, stone.length / 2))}`,
+        timesToBlink - 1
+      ) +
+      blinkAndCountStones(
+        `${Number(stone.slice(stone.length / 2))}`,
+        timesToBlink - 1
+      );
+  }
+
+  globalBlinkCache.set(cacheKey, stonesCreated);
+
+  return stonesCreated;
 }
 
-let stoneCount = 0;
+let stoneCount: number = 0;
+let stoneCount2: number = 0;
+
 for (const stone of stones) {
-  stoneCount += blink(stone, 25);
+  stoneCount += blinkAndCountStones(stone, 25);
+  stoneCount2 += blinkAndCountStones(stone, 75);
 }
 
 console.log(`Part 1 solution: ${stoneCount}`);
+console.log(`Part 1 solution: ${stoneCount2}`);
